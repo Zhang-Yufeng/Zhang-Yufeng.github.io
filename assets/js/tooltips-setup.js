97172ca7f638f33c71c184dd9046cc71b3ef9a65
Yufeng Zhang <v.yfzhang@gmail.com>
2026-01-30 12:48:06 +0800

    Resolve merge confilicts

--- assets/js/tooltips-setup.js
+++ assets/js/tooltips-setup.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-=======
 $(function(){$('[data-toggle="tooltip"]').tooltip()});
->>>>>>> 1ce95abf5eddd8c14d53977c9aafa0272127269c
--- assets/js/wechat.js
+++ assets/js/wechat.js
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
-var wechatModal = document.getElementById("WeChatMod");
-var wechatBtn = document.querySelectorAll('[id="WeChatBtn"]');
-
-for (var i = 0; i < wechatBtn.length; i++) {
-  wechatBtn[i].onclick = function () {
-    wechatModal.style.display = "block";
-  };
-}
-
-window.onclick = function (event) {
-  if (event.target == wechatModal) {
-    wechatModal.style.display = "none";
-  }
-};
-=======
 for(var wechatModal=document.getElementById("WeChatMod"),wechatBtn=document.querySelectorAll('[id="WeChatBtn"]'),i=0;i<wechatBtn.length;i++)wechatBtn[i].onclick=function(){wechatModal.style.display="block"};window.onclick=function(t){t.target==wechatModal&&(wechatModal.style.display="none")};
->>>>>>> 1ce95abf5eddd8c14d53977c9aafa0272127269c
